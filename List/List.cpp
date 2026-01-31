605586e80b6a96dbe57cac974f2608a3a7084344
reverse_1999 <2914824866@qq.com>
2026-01-31 23:25:05 +0800

    ReverseList

--- List/List.cpp
+++ List/List.cpp
@@ -106,6 +106,78 @@ void List_add_in_all(Node* head, int pos)//pos适用于0-length
 		}
 }
 
+void List_delete(Node* head,int idx)//index:1-length
+{
+	if (head == NULL) { printf("error\n");  return; }
+	if(idx<=0) { printf("error\n");  return; }
+	Node* cursor = head;
+	int i = 0;
+
+	while (idx-1 > i && cursor->next != NULL)//定位到idx前一个节点
+	{
+		cursor = cursor->next;  
+		i++;
+	}
+	if(i != idx-1) { printf("error\n");  return; }
+	Node* temp_cursor = cursor->next;
+	if(cursor->next == NULL) { printf("error\n");  return; }
+	cursor->next = cursor->next->next;
+	free(temp_cursor);
+}
+
+int List_find(Node* head,int data)
+{
+	if (head == NULL) { printf("error\n");  return -1; }
+	int idx = 1;
+	Node* cursor = head->next;
+	while (cursor != NULL && data != cursor->data)
+	{
+		idx++;
+		cursor = cursor->next;
+	}
+	if(cursor == NULL) { printf("error\n");  return -1; }
+	return idx;
+}
+
+void List_Reverse(Node* head)//反转链表,带无数据头节点
+{
+	if (head == NULL || head->next == NULL) { printf("error\n");  return; }
+	Node* cursor1 = head->next->next;
+	Node* cursor2 = head->next;
+	Node* cursor3 = head->next;
+	
+	while (cursor1 != NULL)
+	{	
+		cursor2 = cursor1;
+		cursor1 = cursor1->next;
+		
+		cursor2->next = cursor3;
+		cursor3 = cursor2;
+	}
+	head->next->next = NULL;
+	head->next = cursor2;
+	return;
+}
+
+void List_Reverse_2(Node* head)//反转链表,无哨兵头节点
+{
+	if (head == NULL) { printf("error\n");  return; }
+	Node* cursor1 = head->next;
+	Node* cursor2 = head;
+	Node* cursor3 = head;
+
+	while (cursor1 != NULL)
+	{
+		cursor2 = cursor1;
+		cursor1 = cursor1->next;
+
+		cursor2->next = cursor3;
+		cursor3 = cursor2;
+	}
+	head->next = NULL;
+	return;
+}
+
 int main()
 {
 	int len = 10;
@@ -115,7 +187,9 @@ int main()
 	//	List_add_in_end(first);
 	//}
 	List_add_in_all(first, 10);
-
+	List_delete(first, 5);
+	List_Print(first);
+	List_Reverse(first);
 	List_Print(first);
 	Free(first);
 

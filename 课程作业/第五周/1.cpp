6e05f374cc159ae9268cbe52dd6efc7413350285
reverse <2914824866@qq.com>
2026-03-30 15:47:36 +0800

    fifth_2

--- 课程作业/第五周/1.cpp
+++ 课程作业/第五周/1.cpp
@@ -6,9 +6,9 @@ using namespace std;
 string reverseString(const string& str) {
     // 核心代码开始：创建一个字符数组来存储反转后的字符串
     string reversed;
-    for(int i = str.length()-1;i > 0;i--)
+    for (int i = str.length() - 1; i >= 0; i--)
     {
-        reversed.append(str[i]);
+        reversed.append(1,str[i]);
     }
     // 核心代码结束：将字符数组转换为字符串并返回
     return reversed;

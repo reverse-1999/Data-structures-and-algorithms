7aa861a7c8f3100614a51fd0d1c2f1454ac878ad
reverse_1999 <2914824866@qq.com>
2026-05-27 10:17:22 +0800

    thethirtyth

--- 课程作业/第十三周/3.cpp
+++ 课程作业/第十三周/3.cpp
@@ -5,7 +5,31 @@ using namespace std;
 
 int minCostConnectPoints(vector<vector<int>> &points)
 {
-    
+    // Prim算法求平面点的最小生成树总权值
+    int n = points.size();
+    vector<int> minDist(n, 100000); // 每个点到生成树的最小距离
+    vector<bool> visited(n, false);
+    minDist[0] = 0;
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        int u = -1;
+        for (int j = 0; j < n; ++j) {
+            if (!visited[j] && (u == -1 || minDist[j] < minDist[u])) {
+                u = j;
+            }
+        }
+        visited[u] = true;
+        res += minDist[u];
+        for (int v = 0; v < n; ++v) {
+            if (!visited[v]) {
+                int cost = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
+                if (cost < minDist[v]) {
+                    minDist[v] = cost;
+                }
+            }
+        }
+    }
+    return res;
 }
 
 int main()
--- 课程作业/第十三周/Dijkstra.cpp
+++ 课程作业/第十三周/Dijkstra.cpp
@@ -4,65 +4,40 @@ using namespace std;
 
 int shortestPath(int N, int M, int E[][3], int v1, int v2)//顶点、边数、边、起始点、终点
 {
-    int weight[N];
-    int P[N];//未访问
-    int Q[N];//已访问
-    for(int i = 0;i<N;i++)
-    {
+    int weight[N+1];
+    bool P[N+1];
+    for(int i = 0; i <= N; i++) {
         weight[i] = MAX_W;
-        P[i] = 1;
-        Q[i] = 0; 
+        P[i] = false;
     }
-    int points_now = v1;
-    weight[v1] = 0;
-    while(1)
-    {
-        P[points_now] = 0;
-        Q[points_now] = 1;
-        int min = MAX_W;
-        for(int i = 0;i<N;i++)
-        {
-            if(Q[i] == 1)
-            {
-                for(int j = 0;j<M;j++)
-                {
-                    if(E[j][0] == i && P[E[j][1]] == 1)
-                    {
-                        if(weight[i] + E[j][2] < weight[E[j][1]])
-                        {
-                            weight[E[j][1]] = weight[i] + E[j][2];
-                            if(weight[E[j][1]] <= min)
-                            {
-                                min = weight[E[j][1]];
-                                points_now = E[j][1];
-                            }
-                        }
-                    }
+    int start = v1;
+    int end = v2;
+    weight[start] = 0;
 
+    for(int i = 1; i <= N; i++) {
+        int u = -1; 
+        int min = MAX_W;
+        for(int j = 1; j <= N; j++) {
+            if(!P[j] && weight[j] <= min) {
+                min = weight[j];
+                u = j;
+            }
+        }
+        if(u == -1) break;
+        P[u] = true;
+        if(u == end) break;
+
+        for(int j = 0; j < M; j++) {
+            if(E[j][0] == u) {
+                int v = E[j][1];
+                if(!P[v] && weight[u] + E[j][2] < weight[v]) {
+                    weight[v] = weight[u] + E[j][2];
                 }
             }
         }
-
-        // 全局最小未处理节点选择：如果没有新的松弛导致选择，做一次全局扫描
-        int next = -1;
-        int best = MAX_W;
-        for (int i = 0; i < N; ++i) {
-            if (P[i] == 1 && weight[i] < best) {
-                best = weight[i];
-                next = i;
-            }
-        }
-        if (next == -1) {
-            // 没有可达的未处理节点，返回不可达或当前目标距离
-            return (weight[v2] == MAX_W) ? -1 : weight[v2];
-        }
-        points_now = next;
-
-        if(points_now == v2)
-        {
-            return weight[v2];
-        }
     }
+    if(weight[end] == MAX_W) return -1;
+    return weight[end];
 }
 int main()
 {
